f6db26536048aa1c36def6a8360fdf2f236b7d46
AppleMan05 <aupathak1234@gmail.com>
2023-07-30 22:27:17 +0530

    Added a config file for the bot
    You can now set your meme upstreams inside the config.json file.

--- utils/memes_aiohttp_parser.py
+++ utils/memes_aiohttp_parser.py
@@ -1,12 +1,11 @@
 import feedparser
 import aiohttp
+from utils.read_config import getConfig
 
 
 async def storeMemes(sort_algorithm: str):
-    meme_url = ['https://lemmy.ml/feeds/c/memes.xml?sort=',
-                'https://sopuli.xyz/feeds/c/memes.xml?sort=',
-                'https://lemmy.world/feeds/c/lemmyshitpost.xml?sort=',
-                'https://lemmy.blahaj.zone/feeds/c/196.xml?sort=']
+    meme_url = getConfig()["memeUpstreams"]
+    print(meme_url)
     memes = []
     async with aiohttp.ClientSession() as session:
         for item in meme_url:
@@ -27,4 +26,5 @@ async def getMemes():
     meme_dict["TopDay"] = await storeMemes("TopDay")
     meme_dict["New"] = await storeMemes("New")
     meme_dict["MostComments"] = await storeMemes("MostComments")
+    print(meme_dict)
     return meme_dict
